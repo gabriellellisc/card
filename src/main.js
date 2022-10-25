48d95b8c5a3284e8b1a5fd5cbbed94afd7eb0d9a
Gabriel Lellis <gabriellelisc@gmail.com>
2022-10-25 20:33:24 -0300

    adicionado mudança de background dos cartões

--- src/main.js
+++ src/main.js
@@ -2,18 +2,29 @@ import "./css/index.css";
 import IMask from "imask";
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
-const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
 
+const ccNumberTextColor = document.querySelector(".cc-number");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
+const bgColorTotal = document.querySelector(".cc");
 function setCardType(type){
   const colors = {
-    visa: ["#2D57F2", "#436D99"],
-    mastercard: ["#C69347", "#DF6F29"],
-    default: ["black", "gray"]
-  };
+    //visa: ["#2D57F2", "#436D99"],
+    
+    visa: ["none", "none"],
+    mastercard: ["none", "none"],
+    default: ["black", "gray"],
+    valecard: ["red", "yellow"],
+    elo: ["red", "blue"],
+  }
   ccBgColor01.setAttribute("fill", colors[type][0]);
-  console.log(ccBgColor01);
   ccBgColor02.setAttribute("fill", colors[type][1]);
   ccLogo.setAttribute("src", `cc-${type}.svg`);
+  bgColorTotal.style.backgroundImage = `url(cc-bg-${type}.svg)`;
+  if(type === "valecard"){
+    ccNumberTextColor.style.color = "black";
+  }else{
+     ccNumberTextColor.style.color = "white";
+  }
 }
 
 const securityCode = document.querySelector("#security-code");
@@ -47,7 +58,7 @@ const cardNumberPattern = {
     {
       mask: "0000 0000 0000 0000",
       regex: /^4\d{0,15}/,
-      cardtype: "visa"
+      cardtype: "visa",
     },
     {
       mask: "0000 0000 0000 0000",
@@ -55,18 +66,35 @@ const cardNumberPattern = {
       cardtype: "mastercard",
     },
     {
-      mask:"0000 0000 0000 0000",
-      cardtype: "default"
-    }
+      mask: "0000 0000 0000 0000",
+      regex: "^606444|^606458|^606482",
+      cardtype: "valecard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex:
+        "/^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/",
+      cardtype: "elo",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex:
+        "/^8011/",
+      cardtype: "inter",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
   ],
-  
-  dispatch: function(appended, dynamicMasked) {
+
+  dispatch: function (appended, dynamicMasked) {
     const number = (dynamicMasked.value + appended).replace(/\D/g, "")
-    const foundMask = dynamicMasked.compiledMasks.find(function(item){
-      return number.match(item.regex);
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
     })
-    return foundMask;
-  }, 
+    return foundMask
+  },
 }
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
 
@@ -96,15 +124,25 @@ function updateSecurityCode(code){
 
 cardNumberMasked.on("accept", () => {
   const cardType = cardNumberMasked.masked.currentMask.cardtype;
-  console.log(cardNumberMasked)
   setCardType(cardType);
   updateCardNumber(cardNumberMasked.value);
 })
 
 function updateCardNumber(number){
-  const ccNumber = document.querySelector(".cc-number")
-    ccNumber.innerText = number.length === 0 ? "1234 5678 9151 1112" : number;
+  var ccNumber = document.querySelector(".cc-number")
+  verifyCardNumber(number, ccNumber)
 }
+
+function verifyCardNumber(number, ccNumber)
+{
+      if (number.length == 0) {
+        ccNumber.innerText = "1234 5678 91011 1213"
+        bgColorTotal.style.backgroundImage = `url(cc-bg.svg)`
+      } else {
+        ccNumber.innerText = number
+      }
+}
+
 
 expirationDateMasked.on("accept", () => {
   updateExpirationDate(expirationDateMasked.value)
